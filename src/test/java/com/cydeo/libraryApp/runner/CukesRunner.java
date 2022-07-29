f25c2929260119d372640ff843b05100f3ee0749
Ilya Dolzhenko <ilya.dolzhenko@gmail.com>
2022-07-29 11:27:32 -0400

    Refactored Page names to be shorter

--- src/test/java/com/cydeo/libraryApp/runner/CukesRunner.java
+++ src/test/java/com/cydeo/libraryApp/runner/CukesRunner.java
@@ -8,9 +8,10 @@ import org.junit.runner.RunWith;
 @CucumberOptions (
         plugin = {
                 "pretty", //Prints additional information about the Scenario that is being executed
+                "json:target/cucumber.json", //Creates cucumber report for Jenkins with .json
                 "html:target/cucumber-reports.html", //Makes HTML reports
                 "rerun:target/rerun.txt", //Keeps tracking of FAILED Scenarios in .txt file
-                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber" //Creates Cucumber report
         },
         features = "src/test/resources/features",
         glue = "com/cydeo/libraryApp/step_definitions",
